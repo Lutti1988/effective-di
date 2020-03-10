a48bc1228864c849949582ef464ef34496fe2431
Francesc Castells <francesc@chamatheapp.com>
2020-03-10 20:42:06 +0100

    Working implementation (without performance improvements)

--- DI_Autofac_Test/Service/EventPublisherService.cs
+++ DI_Autofac_Test/Service/EventPublisherService.cs
@@ -3,37 +3,34 @@ using DI_Autofac_Test.Eventhandler;
 using DI_Autofac_Test.Events;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DI_Autofac_Test.Service
 {
 	public class EventPublisherService : IEventPublisherService
 	{
-		private readonly IEnumerable<IEventHandler<IEvent>> _eventHandlers;
+        private readonly ILifetimeScope _lifetimeScope;
 
-		public EventPublisherService(IContainer container)
+		public EventPublisherService(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+		public async Task Emit(IEvent eventItem)
 		{
-			Type ienumerableOfIEventHandlerType = typeof(IEnumerable<>).MakeGenericType(typeof(IEventHandler<>));
-			IEnumerable<IEvent> eventHandler = container.Resolve(ienumerableOfIEventHandlerType) as IEnumerable<IEvent>;
-			// The code line above results in the following error: 
-			/* Autofac.Core.Registration.ComponentNotRegisteredException: 'The requested service '' has not been registered. 
-			   To avoid this exception, either register a component to provide the service, check for service registration using IsRegistered(), 
-			   or use the ResolveOptional() method to resolve an optional dependency. */
+            Type[] typeArgs = { eventItem.GetType() };
+            Type handlerType = typeof(IEventHandler<>).MakeGenericType(typeArgs);
+			Type handlerCollectionType = typeof(IEnumerable<>).MakeGenericType(handlerType);
+			
+			var handlers = (IEnumerable<object>)_lifetimeScope.Resolve(handlerCollectionType);
 
-
-			/*
-			Type generic = typeof(IEnumerable<IEventHandler<>>); // Unexpected use of an unbound generic 
-			Type[] typeArgs = { typeof(IEvent) }; // TEvent is always a type of IEvent
-			Type constructed = generic.MakeGenericType(typeArgs);
-
-			_eventHandlers = container.Resolve(constructed);
-			*/
-
-		}
-
-		public Task Emit(IEvent eventItem)
-		{
-			return Task.CompletedTask;
-		}
+            var handleMethod = handlerType.GetMethod("Handle");
+			
+			foreach (object handler in handlers)
+			{
+				await ((Task)handleMethod.Invoke(handler, new object[] { eventItem }));
+			}
+        }
 	}
 }
--- DI_Autofac_Test/TypeExtensions.cs
+++ DI_Autofac_Test/TypeExtensions.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
 
-namespace DI_Autofac_Test
-{
-	public static class TypeExtensions
-	{
-		public static Type GetGenericInterfaceType(this Type source, Type expectedGenericInterfaceType)
-		{
-			return source.GetInterfaces()
-				.First(
-					interfaceItem => interfaceItem.IsGenericType
-					&& interfaceItem.GetGenericTypeDefinition().Equals(expectedGenericInterfaceType)
-				);
-		}
+//namespace DI_Autofac_Test
+//{
+//	public static class TypeExtensions
+//	{
+//		public static Type GetGenericInterfaceType(this Type source, Type expectedGenericInterfaceType)
+//		{
+//			return source.GetInterfaces()
+//				.First(
+//					interfaceItem => interfaceItem.IsGenericType
+//					&& interfaceItem.GetGenericTypeDefinition().Equals(expectedGenericInterfaceType)
+//				);
+//		}
 
-		public static List<Type> GetTypesWithGenericInterfacesInAssemblies(this Type source, params Assembly[] assemblies)
-		{
-			if (assemblies == null || assemblies.Length == 0) { throw new Exception(); }
+//		public static List<Type> GetTypesWithGenericInterfacesInAssemblies(this Type source, params Assembly[] assemblies)
+//		{
+//			if (assemblies == null || assemblies.Length == 0) { throw new Exception(); }
 
-			return assemblies
-				.SelectMany(currentAssembly => currentAssembly.GetTypes()
-				.Where(type => type.GetInterfaces().Any(
-					interfaceItem => interfaceItem.IsGenericType
-					&& interfaceItem.GetGenericTypeDefinition().Equals(source))))
-				.ToList();
-		}
-	}
-}
+//			return assemblies
+//				.SelectMany(currentAssembly => currentAssembly.GetTypes()
+//				.Where(type => type.GetInterfaces().Any(
+//					interfaceItem => interfaceItem.IsGenericType
+//					&& interfaceItem.GetGenericTypeDefinition().Equals(source))))
+//				.ToList();
+//		}
+//	}
+//}
